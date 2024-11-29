261dbdb3eb8e749af6ef023f4f0c665aff9d7253
Carlos de Jesús Hernández García <93212816+CarlosHG04@users.noreply.github.com>
2024-11-28 21:27:22 -0600

    online-js-create 4.0.1
    Es un solo archi no requiere cambios

---
+++ script.js
@@ -0,0 +1,6 @@
+// Ejemplo básico de interactividad con JS
+// Puedes agregar más funciones aquí según lo que quieras lograr
+
+document.querySelector('button').addEventListener('click', function() {
+    alert('Gracias por tu compra!'); // Esto es solo un ejemplo
+});
